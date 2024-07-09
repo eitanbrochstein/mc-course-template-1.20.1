9e1aae240120116217c9aff1667ad6216b92be79
25ejsb <eitan.brochstein@icloud.com>
2024-07-09 18:54:01 -0400

    lol

---
+++ src/main/java/net/eitan/mccourse/block/ModBlocks.java
@@ -0,0 +1,10 @@
+package net.eitan.block;
+
+import net.eitan.mccourse.McCourse;
+
+public class ModBlocks {
+
+    public static void registerModBlocks() {
+        McCourse.LOGGER.info("Registering ModBlocks for " + McCourse.MOD_ID);
+    }
+}
---
+++ src/main/java/net/eitan/mccourse/item/ModItems.java
@@ -0,0 +1,2 @@
+package net.eitan.mccourse.item;public class items {
+}
