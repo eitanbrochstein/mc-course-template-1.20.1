fcb5e3eab242139e2c7d62a1af8edee37e180086
25ejsb <eitan.brochstein@icloud.com>
2024-08-01 18:28:09 -0400

    lol

--- src/main/java/net/eitan/mccourse/util/ModRegistries.java
+++ src/main/java/net/eitan/mccourse/util/ModRegistries.java
@@ -2,15 +2,22 @@ package net.eitan.mccourse.util;
 
 import net.eitan.mccourse.item.ModItems;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.block.ComposterBlock;
 
 public class ModRegistries {
     public static void registerModStuffs() {
         registerFuels();
+        registerCompostables();
     }
 
     private static void registerFuels() {
         FuelRegistry registry = FuelRegistry.INSTANCE;
 
         registry.add(ModItems.PEAT_BRICK, 200);
+    }
+
+    private static void registerCompostables() {
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER, 0.5f);
+        ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER_SEEDS, 0.25f);
     }
 }
