7e9c26d1b2af44a0c411dcef318aaaefbfed1360
25ejsb <eitan.brochstein@icloud.com>
2024-08-06 12:37:01 -0400

    lol

---
+++ src/main/java/net/eitan/mccourse/event/PlayerCopyHandler.java
@@ -0,0 +1,11 @@
+package net.eitan.mccourse.event;
+
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class PlayerCopyHandler implements ServerPlayerEvents.CopyFrom {
+    @Override
+    public void copyFromPlayer(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean alive) {
+
+    }
+}
--- src/main/java/net/eitan/mccourse/util/ModRegistries.java
+++ src/main/java/net/eitan/mccourse/util/ModRegistries.java
@@ -1,6 +1,8 @@
 package net.eitan.mccourse.util;
 
+import net.eitan.mccourse.event.AttackEntityHandler;
 import net.eitan.mccourse.item.ModItems;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.block.ComposterBlock;
 
@@ -8,6 +10,7 @@ public class ModRegistries {
     public static void registerModStuffs() {
         registerFuels();
         registerCompostables();
+        registerEntity();
     }
 
     private static void registerFuels() {
@@ -19,5 +22,9 @@ public class ModRegistries {
     private static void registerCompostables() {
         ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER, 0.5f);
         ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE.put(ModItems.CAULIFLOWER_SEEDS, 0.25f);
+    }
+
+    private static void registerEntity() {
+        AttackEntityCallback.EVENT.register(new AttackEntityHandler());
     }
 }
